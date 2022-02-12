924fdbda8215d67a4233fdab94911bdfa9c03f0e
Stas74 <splotnikov74@gmail.com>
2022-02-11 18:03:00 -0700

    eslint

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,10 +16,10 @@ const pool = new Pool({
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
- */ 
+ */
 
-const getUserWithEmail = (email) => {  
-  return pool  
+const getUserWithEmail = (email) => {
+  return pool
     .query(`SELECT * FROM users WHERE email = $1`, [email])
     .then((result) => {
       return result.rows[0]})
@@ -35,15 +35,15 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 
-const getUserWithId = (id) => {  
-  return pool  
+const getUserWithId = (id) => {
+  return pool
     .query(`SELECT * FROM users WHERE id = $1`, [id])
     .then((result) => {
       return result.rows[0]})
     .catch((err) => {
       console.log(err.message);
   });
-}
+};
 
 exports.getUserWithId = getUserWithId;
 
@@ -54,16 +54,16 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 
-const addUser =  function(user) {    
-  return pool  
-  .query(`INSERT INTO users (name, email, password) 
+const addUser =  function(user) {
+  return pool
+  .query(`INSERT INTO users (name, email, password)
           VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
-  .then((result) => {    
+  .then((result) => {
     return result.rows})
   .catch((err) => {
     console.log(err.message);
   });
-}
+};
 
 exports.addUser = addUser;
 
@@ -79,12 +79,12 @@ exports.addUser = addUser;
 const getAllReservations = function(guest_id, limit = 10) {
   return pool
     .query(`SELECT * FROM reservations WHERE guest_id = $1 LIMIT 10`, [guest_id])
-    .then((result) => {    
+    .then((result) => {
       return result.rows})
     .catch((err) => {
       console.log(err.message);
   });
-}
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -96,7 +96,7 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
-const getAllProperties = function (options, limit = 10) {  
+const getAllProperties = function(options, limit = 10) {
   const queryParams = [];
   // 2
   let queryString = `
@@ -115,15 +115,15 @@ const getAllProperties = function (options, limit = 10) {
   if (options.owner_id) {
     queryParams.push(owner_id);
     queryString += `AND owner_id = $${queryParams.length} `;
-  }  
+  }
   //  minimum_price_per_night
   if (options.minimum_price_per_night) {
-    queryParams.push(options.minimum_price_per_night * 100);    
+    queryParams.push(options.minimum_price_per_night * 100);
     queryString += `AND cost_per_night > $${queryParams.length}`;
   }
   // maximum_price_per_night
   if (options.maximum_price_per_night) {
-    queryParams.push(options.maximum_price_per_night * 100);    
+    queryParams.push(options.maximum_price_per_night * 100);
     queryString += `AND cost_per_night < $${queryParams.length}`;
   }
 
@@ -148,9 +148,6 @@ const getAllProperties = function (options, limit = 10) {
   // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
-
-
-
 exports.getAllProperties = getAllProperties;
 
 
@@ -159,15 +156,15 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = (property) => {  
+const addProperty = (property) => {
   return pool.query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, 
     cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, 
-    country, street, city, province, post_code) 
+    country, street, city, province, post_code)
     VALUES  ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
     RETURNING *;`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,
     property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms,
     property.country, property.street, property.city, property.province, property.post_code])
     .then(res => res.rows[0])
-    .catch(err => err.message);  
-}
+    .catch(err => err.message);
+};
 exports.addProperty = addProperty;
